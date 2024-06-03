5ca9867042c4de2a5018b01587342291e0608263
Matthew Castiglione <matt3castiglione@sbcglobal.net>
2024-06-03 11:03:00 -0500

    Finished project. Prints Fibonacci Sequence to Nth Number.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,42 @@
 namespace Fibonacci{
     class Fibonacci(){
         public static void Main(string[] args){
+            Greetings();
+            string? userInput = Console.ReadLine();
+            Console.WriteLine(Fibo(userInput));
+        }
+
+        static void Greetings(){
+            Console.WriteLine("Hello! This app will print out the Fibonacci Sequence to any number you feed it! Make sure it is an integer!");
+        }
+
+        static int Fibo(string? userInput){
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            int nth = 0;
+
+            if(int.TryParse(userInput, out int i)){
+                nth = Convert.ToInt32(userInput);
+
+                if (nth == 0)
+                {
+                    return a;
+                }
+                else {
+                    for (int x = 2; x <= nth; x++){
+                        c = a + b;
+                        a = b;
+                        b = c;
+                    }
+                    return b;
+                }
+            }
+            else{
+                Console.WriteLine("ERROR: Not an integer!");
+                return 0;
+            }
         }
     }
 }
