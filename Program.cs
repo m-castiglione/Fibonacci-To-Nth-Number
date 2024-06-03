fcb6b3ab990fc103d98b863a6751d4251db83142
Matthew Castiglione <matt3castiglione@sbcglobal.net>
2024-06-03 11:01:53 -0500

    Initial commit. Project initialization.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Fibonacci{
+    class Fibonacci(){
+        public static void Main(string[] args){
+        }
+    }
+}
